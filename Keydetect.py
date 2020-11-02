70bb4ae666820e7c62b6fd585d63c7b11cf829eb
theplower <40697626+theplower@users.noreply.github.com>
2020-11-02 23:50:46 +0200

    Update Keydetect.py

--- Keydetect.py
+++ Keydetect.py
@@ -1,3 +1,4 @@
+# Import the required libraries
 import cv2 as cv
 import torch
 import torchvision
@@ -7,36 +8,43 @@ import pyautogui
 from torch import nn
 from datetime import datetime
 
+# Record the time taken to load the model
 a = datetime.now()
-
-
+# Define the path to the trained model and load it 
 PATH = "Keymodel.pt" #"80 epoch weight with dropout.pt"#"81-87transfered.pt"
 model = torch.load(PATH,torch.device("cpu"))
-
 b = datetime.now()
+
+# Set the model to the prediction mode
 model.eval()
-cap=cv.VideoCapture(sys.argv[1])
+
+# Define the video stream input
+cap = cv.VideoCapture(sys.argv[1])
+
 print('Model loaded in ', (b - a))
 
+# Define the classes
 class_names = ['down', 'left', " ", 'right', 'up']
-frameCount = 0
-frameStep = 24
+
+# Open the window used to show the frames
 cv.namedWindow('Window')
 font = cv.FONT_HERSHEY_SIMPLEX
-placeholder = None
+
+# Define the coords for the green rectangle that must enclose the hand direction
 x2 = int(cap.get(3) / 2) + 20
 y2 = int(cap.get(4) / 2) + 20
-while True:
 
-	_, origFrame = cap.read()
-#	frame = cv.flip(frame,1)
-#	origFrame = cv.resize(frame, (480, 360))
-#	frame = cv.resize(frame, (480, 360))
-	x1, y1 = (0, 0)
-	
-#	if frameCount % frameStep == 0:	
+while True:
+	# Read the video frame by frame
+	success, origFrame = cap.read()
+	# Assert that the stream isn't cut
+	if not success:
+		break
+	# Extract the hand	
 	frame = origFrame[0: x2, 0: y2]
-	cut = frame = cv.resize(frame, (64,64))
+	
+	# Set the extracted part of the image to the form known to the model
+	frame = cv.resize(frame, (64,64))
 	frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 	#converting it to the form that is known to the model
 	frame = np.array(frame,dtype=np.float32)
@@ -44,13 +52,14 @@ while True:
 	frame = (frame - 0.5) / 0.5
 	frame = torch.tensor(frame)
 	
-	#getting the sign meaning 
+	# Get the sign meaning 
 	prediction = model(frame.view(1,1,64,64))
 	confidences = torch.softmax(prediction.detach(),1)
 	confidencePercent = np.amax(confidences.numpy(),1).item()
 	if confidencePercent > 0.2:
 		classIndex = np.argmax(prediction.detach().numpy())
 		letter = class_names[classIndex]
+		# Decide which key to press
 		if letter == 'up':
 			pyautogui.press('UP')
 		elif letter == 'down':
@@ -62,12 +71,11 @@ while True:
 		message = letter
 	else:
 		message = ' '
-#		placeholder = message
-#else:
-#	message = placeholder
+	# Show the recognized direction on the video (or Live Camera stream)
 	cv.putText(origFrame, message, (20, 25), font, 0.75, (255, 0, 0), 2, cv.LINE_AA)
 	cv.rectangle(origFrame, (0, 0), (x2, y2), (0, 255, 0), 3)
-#	origFrame = cv.flip(origFrame, 1)
+	# Uncomment the next line if the image is flipped
+	# origFrame = cv.flip(origFrame, 1)
 	cv.imshow('Window', origFrame)
 	if cv.waitKey(1) & 0xFF == ord('q'):
 		break
